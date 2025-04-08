a0c6cabbec9991face1af42e97837722895efc8e
namth1793 <namtranhoai179.3@gmail.com>
2025-04-08 15:26:57 +0700

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const RX_CHAR_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'; // Web sends -> ESP
 async function connectBLE() {
     bleDevice = await navigator.bluetooth.requestDevice({
         filters: [{
-            namePrefix: "ESP32S3"
+            namePrefix: "SpiderXBot"
         }],
         optionalServices: [NUS_SERVICE_UUID]
     });
